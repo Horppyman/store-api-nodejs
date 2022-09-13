13687c33b31b509e313fc19a6a1a3b34907f31ed
horppyman <horppyboi1@gmail.com>
2022-09-13 08:40:01 +0100

    project barebone

---
+++ app.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+const connectDB = require("./db/connect");
+const productsRouter = require('./routes/products')
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/product">Products route</a>');
+});
+
+app.use('/api/v1/products', productsRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server listening on port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
