702c850027c82d1a8d1606141e0fb7d28cc94666
horppyman <horppyboi1@gmail.com>
2022-09-16 09:23:48 +0100

    product cintroller and model update

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
+require("express-async-errors");
 
 const express = require("express");
 const app = express();
 
 const connectDB = require("./db/connect");
-const productsRouter = require('./routes/products')
+const productsRouter = require("./routes/products");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
@@ -14,7 +15,7 @@ app.get("/", (req, res) => {
   res.send('<h1>Store API</h1><a href="/api/v1/product">Products route</a>');
 });
 
-app.use('/api/v1/products', productsRouter);
+app.use("/api/v1/products", productsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
